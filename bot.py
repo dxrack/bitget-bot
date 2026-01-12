ac7641393930169a97c50255ecd1d12264ca2463
dxrack <dxrack@hanmail.net>
2026-01-12 19:26:53 +0900

    Add bot

---
+++ bot.py
@@ -0,0 +1,27 @@
+﻿import os
+import time
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s | %(levelname)s | %(message)s',
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+
+api_key = os.environ.get('BITGET_API_KEY')
+secret_key = os.environ.get('BITGET_SECRET_KEY')
+passphrase = os.environ.get('BITGET_PASSPHRASE')
+
+logging.info('Bot Starting')
+
+if api_key and secret_key and passphrase:
+    logging.info('API credentials OK')
+    logging.info('Bot is RUNNING')
+else:
+    logging.error('Missing credentials')
+    sys.exit(1)
+
+while True:
+    time.sleep(60)
+    logging.info('Status OK')
